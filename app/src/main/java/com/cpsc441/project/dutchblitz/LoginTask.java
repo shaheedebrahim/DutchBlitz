37798b34e8e146ae37f85b0e36f485adf2418515
Sean Ovens <seangovens@gmail.com>
2016-03-21 19:34:30 -0600

    Server updated to TCP non-blocking server.

--- app/src/main/java/com/cpsc441/project/dutchblitz/LoginTask.java
+++ app/src/main/java/com/cpsc441/project/dutchblitz/LoginTask.java
@@ -6,10 +6,14 @@ import android.util.Log;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.Socket;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 
@@ -17,7 +21,9 @@ public class LoginTask extends AsyncTask<String, Void, String> {
     final int PACKET_SIZE = 64;
 
     private Context appCon = null;
-    private DatagramSocket sock = null;
+    private Socket sock = null;
+    private DataOutputStream out = null;
+    private BufferedReader in = null;
 
     public LoginTask(Context con) {
         appCon = con;
@@ -25,30 +31,32 @@ public class LoginTask extends AsyncTask<String, Void, String> {
 
     @Override
     protected void onPreExecute() {
-        try {
-            sock = new DatagramSocket();
-            Log.d("Init: ", "Success");
-        }
-        catch (SocketException e) {
-            System.out.println("Failed to create client socket.");
-            e.printStackTrace();
-        }
     }
 
     @Override
     protected String doInBackground(String... params) {
+        try {
+            sock = new Socket("192.168.56.1", 1234);
+            out = new DataOutputStream(sock.getOutputStream());
+            in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+            Log.d("Init: ", "Success");
+        }
+        catch (UnknownHostException e) {
+            System.out.println("Failed to create client socket.");
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            System.out.println("Socket creation caused error.");
+            e.printStackTrace();
+        }
+
         String message = params[0] + " " + params[1], resp = "";
 
         try {
-            byte[] outBuff = message.getBytes();
             // Send credentials to server - IP address is currently hard-coded
-            DatagramPacket pack = new DatagramPacket(outBuff, outBuff.length, InetAddress.getByName("192.168.1.109"), 9876);
-            sock.send(pack);
+            out.writeBytes(message);
 
-            byte[] inBuff = new byte[PACKET_SIZE];
-            pack = new DatagramPacket(inBuff, inBuff.length);
-            sock.receive(pack);
-            resp = new String(pack.getData());
+            resp = in.readLine();
         }
         catch (UnknownHostException e) {
             System.out.println("Attempted to contact unknown host.");
@@ -66,7 +74,12 @@ public class LoginTask extends AsyncTask<String, Void, String> {
     @Override
     protected void onPostExecute(String res) {
         Log.d("Android: ", "Exchange done");
-        sock.close();
+        try {
+            sock.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
         Toast.makeText(appCon, res, Toast.LENGTH_LONG).show();
     }
 }
--- backend/Server.java
+++ backend/Server.java
@@ -1,53 +1,144 @@
-import java.net.*;
+/*
+ * A simple TCP select server that accepts multiple connections and echo message back to the clients
+ * For use in CPSC 441 lectures
+ * Instructor: Prof. Mea Wang
+ */
+
 import java.io.*;
+import java.net.*;
 import java.nio.*;
-import java.nio.charset.*;
 import java.nio.channels.*;
+import java.nio.charset.*;
+import java.util.*;
 
 public class Server {
-    final static int PACKET_SIZE = 64;
-    
-    private static DatagramChannel channel = null;
-    private static Charset charset = Charset.forName( "us-ascii" );  
-    private static CharsetDecoder decoder = charset.newDecoder();  
-    private static ByteBuffer inBuffer;
-    private static CharBuffer cBuffer;
-    
-    public static void main(String[] args) throws IOException {
-        channel = DatagramChannel.open();
-        InetSocketAddress isa = new InetSocketAddress(9876);
+    public static int BUFFERSIZE = 32;
+    public static void main(String args[]) throws Exception
+    {
+        if (args.length != 1)
+        {
+            System.out.println("Usage: UDPServer <Listening Port>");
+            System.exit(1);
+        }
+
+        // Initialize buffers and coders for channel receive and send
+        String line = "";
+        Charset charset = Charset.forName( "us-ascii" );
+        CharsetDecoder decoder = charset.newDecoder();
+        CharsetEncoder encoder = charset.newEncoder();
+        ByteBuffer inBuffer = null;
+        CharBuffer cBuffer = null;
+        int bytesSent, bytesRecv;     // number of bytes sent or received
+
+        // Initialize the selector
+        Selector selector = Selector.open();
+
+        // Create a server channel and make it non-blocking
+        ServerSocketChannel channel = ServerSocketChannel.open();
+        channel.configureBlocking(false);
+
+        // Get the port number and bind the socket
+        InetSocketAddress isa = new InetSocketAddress(Integer.parseInt(args[0]));
         channel.socket().bind(isa);
-        
-        byte[] inBuff = new byte[PACKET_SIZE];
-        DatagramPacket pack = new DatagramPacket(inBuff, PACKET_SIZE);
-        
-        String in = "";
-        while (true) {
-            inBuffer = ByteBuffer.allocateDirect(PACKET_SIZE);
-			cBuffer = CharBuffer.allocate(PACKET_SIZE);
-            
-            SocketAddress client = channel.receive(inBuffer);
-            
-            inBuffer.flip();
-            decoder.decode(inBuffer, cBuffer, false);
-            cBuffer.flip();
-            
-            in = cBuffer.toString();
-            String[] fields = in.split(" ");
-            
-            System.out.println("Client: " + in);
-            
-            String resp = "";
-            if (fields.length == 2 && fields[0].equals("admin") && fields[1].equals("admin"))
-                resp = "success";
-            else resp = "failure";
-            
-            byte[] rBytes = resp.getBytes();
-            inBuffer = ByteBuffer.allocateDirect(rBytes.length);
-            inBuffer.put(rBytes);
-            inBuffer.flip();
-            
-            channel.send(inBuffer, client);
+
+        // Register that the server selector is interested in connection requests
+        channel.register(selector, SelectionKey.OP_ACCEPT);
+
+        // Wait for something happen among all registered sockets
+        try {
+            boolean terminated = false;
+            while (!terminated)
+            {
+                if (selector.select(500) < 0)
+                {
+                    System.out.println("select() failed");
+                    System.exit(1);
+                }
+
+                // Get set of ready sockets
+                Set readyKeys = selector.selectedKeys();
+                Iterator readyItor = readyKeys.iterator();
+
+                // Walk through the ready set
+                while (readyItor.hasNext())
+                {
+                    // Get key from set
+                    SelectionKey key = (SelectionKey)readyItor.next();
+
+                    // Remove current entry
+                    readyItor.remove();
+
+                    // Accept new connections, if any
+                    if (key.isAcceptable())
+                    {
+
+                        SocketChannel cchannel = ((ServerSocketChannel)key.channel()).accept();
+                        cchannel.configureBlocking(false);
+                        System.out.println("Accept conncection from " + cchannel.socket().toString());
+
+                        // Register the new connection for read operation
+                        cchannel.register(selector, SelectionKey.OP_READ);
+                    }
+                    else
+                    {
+                        SocketChannel cchannel = (SocketChannel)key.channel();
+                        if (key.isReadable())
+                        {
+                            Socket socket = cchannel.socket();
+
+                            // Open input and output streams
+                            inBuffer = ByteBuffer.allocateDirect(BUFFERSIZE);
+                            cBuffer = CharBuffer.allocate(BUFFERSIZE);
+
+                            // Read from socket
+                            bytesRecv = cchannel.read(inBuffer);
+                            if (bytesRecv <= 0)
+                            {
+                                System.out.println("read() error, or connection closed");
+                                key.cancel();  // deregister the socket
+                                continue;
+                            }
+
+                            inBuffer.flip();      // make buffer available  
+                            decoder.decode(inBuffer, cBuffer, false);
+                            cBuffer.flip();
+                            line = cBuffer.toString() + "\n";
+                            System.out.print("Client: " + line);
+
+                            // Echo the message back
+                            inBuffer = ByteBuffer.allocateDirect(BUFFERSIZE);
+                            inBuffer.put("Success\n".getBytes());
+                            inBuffer.flip();
+                            bytesSent = cchannel.write(inBuffer);
+                            /*if (bytesSent != bytesRecv)
+                            {
+                                System.out.println("write() error, or connection closed");
+                                key.cancel();  // deregister the socket
+                                continue;
+                            }*/
+
+                            if (line.equals("terminate\n"))
+                                terminated = true;
+                        }
+                    }
+                } // end of while (readyItor.hasNext()) 
+            } // end of while (!terminated)
+        }
+        catch (IOException e) {
+            System.out.println(e);
+        }
+
+        // close all connections
+        Set keys = selector.keys();
+        Iterator itr = keys.iterator();
+        while (itr.hasNext())
+        {
+            SelectionKey key = (SelectionKey)itr.next();
+            //itr.remove();
+            if (key.isAcceptable())
+                ((ServerSocketChannel)key.channel()).socket().close();
+            else if (key.isValid())
+                ((SocketChannel)key.channel()).socket().close();
         }
     }
 }
